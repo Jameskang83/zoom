4d83ca397c56ed493f87893c67204d6ddfccad10
jimkang83 <jim.kang83@gmail.com>
2023-02-18 01:02:14 -0700

    1.9 Nicknames

--- src/server.js
+++ src/server.js
@@ -16,13 +16,27 @@ const handleListen = () => console.log("Listening on http://loaclhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
-  console.log("Connected to Server");
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
   socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-  socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
+    // console.log(parsed, message.toString("utf8"));
   });
-  socket.send("hello");
 });
 
 server.listen(3000, handleListen);
